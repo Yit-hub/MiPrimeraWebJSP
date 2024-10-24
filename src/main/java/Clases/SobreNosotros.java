e4a57a88874b19467a8ce9e070901a2c5b3a5523
Yit-hub <yidanielchen.23@campuscamara.es>
2024-10-24 14:14:13 +0200

    a

---
+++ src/main/java/Clases/SobreNosotros.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Yi Daniel Chen Liu
+ */
+public class SobreNosotros {
+    public static void info(){
+        System.out.println("Somos una empresa especializada en propagar plagas en el entorno Urbano");
+    }
+    public static void lema(){
+        System.out.println("¿Estás cansado de comparar siempre tus asquerosas plantas con las maravillosas plantas de tu vecino? "
+                + "No lo pienses más contrata PLAGATOR, y será tu vecino el que compare sus asquerosas plantas con las tuyas");
+    }
+}
